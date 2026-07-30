e9b6d72498584a403a6665bcf28e5a1f6917ff01
thesimas <luciano.simasjr@gmail.com>
2026-07-30 16:19:00 -0300

    Fix: Ajustando a lógica e melhorando o layout das strings no terminal

--- aulas/2026-07-29-aula/Data.java
+++ aulas/2026-07-29-aula/Data.java
@@ -1,77 +1,132 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Data {
-    void main(){
-        // Faça um programa que o usuario deve informar: 
-        // - Sigla da disciplina 
-        // - Todos os dias da semana que tem aula dessa discplina 
+    void main() {
+        // Faça um programa que o usuario deve informar:
+        // - Sigla da disciplina
+        // - Todos os dias da semana que tem aula dessa discplina
         // Por fim, imprima:
-        // Na disciplina *** você tem aula nos seguintes dias: 
-        // - Segunda 
-        // - Quarta 
+        // Na disciplina *** você tem aula nos seguintes dias:
+        // - Segunda
+        // - Quarta
         // - Sexta
-        
+
         Scanner leia = new Scanner(System.in);
         String[] diasInformados = new String[5];
-        int index = 0;
+        String[] diasValidos = new String[] { "segunda", "terca", "quarta", "quinta", "sexta" };
         String resposta = "";
-        System.out.println("\n\nQual é a sigla da sua Disciplina?");
-        String siglaDisciplina = leia.nextLine();
+        String siglaDisciplina = "";
+        int index = 0;
 
-        System.out.println("\n\nVocê tem aula em quais dias?");
-        String[] diasValidos = new String[] {"segunda", "terça", "quarta", "quinta", "sexta"};
+        instrucoes(diasValidos);
+        int quantidadeDeDisciplinas = disciplinas(leia);
 
-        while (true) {
+        while (quantidadeDeDisciplinas > 0) {
 
-            if (index > 4){
-                break;
-            }
+            System.out.println("Qual é a sigla da sua Disciplina?");
+            siglaDisciplina = leia.nextLine().toUpperCase();
 
-            resposta = leia.nextLine().toLowerCase();
-
-            if(resposta.charAt(0) == 'n'){
-                break;
-            }
-
+            System.out.println("Você tem aula em quais dias?");
             while (true) {
-                diasInformados[index] = resposta;
 
-                if(!(resposta.contains(diasValidos[index]))){
-                    if(verificador(diasInformados, resposta)){
-                        IO.println("Dia informado já foi preenchido!");
-                    }else{
-                        System.out.println("Dia incorreto, informe: ");
-                    }
-                    IO.println("\nInforme: ");
-                    for(int x = 0; x < diasValidos.length; x ++){
-                        IO.print(diasValidos[x] + " ");
-                    }
-                    IO.println();
-                    resposta = leia.nextLine().toLowerCase();
-                }else{
+                if (index > 4) {
                     break;
                 }
-            }
-            
-            index ++;
-        }
 
-        IO.println("\nDiscplina: "+ siglaDisciplina +"\nDias de Aula na Semana: ");
-        for(int x = 0; x < index; x ++){
-            IO.print(diasInformados[x] + " ");
+                resposta = leia.nextLine().toLowerCase();
+
+                if (resposta.charAt(0) == 'n') {
+                    break;
+                }
+
+                if (verificadorDiaValido(resposta, diasValidos)
+                        && verificadorDiasDuplicados(resposta, diasInformados)) {
+                    diasInformados[index] = resposta;
+                    index++;
+                } else {
+                    if (!verificadorDiasDuplicados(resposta, diasInformados)) {
+                        System.out.println(diasInformados[index] + " já foi informado.");
+                    } else {
+                        System.out.println("Dia informado não existe!");
+                    }
+                }
+            }
+
+            if (diasInformados.length != 0) {
+                mostraDias(siglaDisciplina, diasInformados, index);
+            }
+
+            index = 0;
+            Arrays.fill(diasInformados, "");
+            quantidadeDeDisciplinas--;
         }
-        IO.println("\n"); 
+        leia.close();
     }
 
-    public static boolean verificador (String[] diasInformados, String resposta){
-        boolean flag = true;
-        for(int x = 0; x < diasInformados.length; x ++){
-            if(resposta.equalsIgnoreCase(diasInformados[x])){
-                flag = false;
+    public static void instrucoes(String[] diasValidos) {
+        System.out.print("\n\nBem vindo ao Programa que irá listar suas aulas por disciplina!\nDias na semana possíveis:");
+        diasPossiveis(diasValidos);
+        System.out.println("Caso queira parar de informar, digite 'não'!\n");
+    }
+
+    public static boolean verificadorDiaValido(String resposta, String[] diasValidos) {
+        int flag = 0;
+
+        for (int x = 0; x < diasValidos.length; x++) {
+            if (resposta.equalsIgnoreCase(diasValidos[x])) {
+                flag += 1;
+            }
+        }
+
+        if (flag != 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean verificadorDiasDuplicados(String resposta, String[] diasInformados) {
+        int flag = 0;
+
+        if (diasInformados.length == 1) {
+            return true;
+        }
+
+        for (int x = 0; x < diasInformados.length; x++) {
+            if (diasInformados[x] != null) {
+                if (resposta.equalsIgnoreCase(diasInformados[x])) {
+                    flag += 1;
+                }
+            } else {
                 break;
             }
         }
-        return flag;
+
+        if (flag > 0) {
+            return false;
+        }
+        return true;
     }
-    
+
+    public static int disciplinas(Scanner leia) {
+        System.out.println("\nQuantas disciplinas você irá cursar?");
+        int disciplinas = leia.nextInt();
+        leia.nextLine();
+        return disciplinas;
+    }
+
+    public static void mostraDias(String sigla, String[] diasInformados, int index) {
+        System.out.println("\nDiscplina: " + sigla + ", dias de aula na Semana: \n");
+        for (int x = 0; x < index; x++) {
+            System.out.println("- " + diasInformados[x].toUpperCase());
+        }
+        System.out.println();
+    }
+
+    public static void diasPossiveis(String[] diasValidos) {
+        for (int x = 0; x < diasValidos.length; x++) {
+            System.out.print("| " + diasValidos[x].toUpperCase() + " ");
+        }
+        System.out.print("|\n");
+    }
 }
